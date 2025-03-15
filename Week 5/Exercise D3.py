74ab072b31bf38034da6de1f47473e160b12891c
Michael <maicolje11@gmail.com>
2025-03-15 15:10:32 -0600

    Ejercicios completos de semana 1 a 8

--- Week 5/Exercise D3.py
+++ Week 5/Exercise D3.py
@@ -1,9 +1,5 @@
-the_greatest_number = 0
-ten_numbers_list = []
-for index in range(10):
-    number = int(input(f"Enter number {index+1}: "))
-    ten_numbers_list.append(number)
-    if the_greatest_number <= number:
-        the_greatest_number = number
-print(f"List of numbers:{ten_numbers_list}")
-print(the_greatest_number) 
+list_of_keys = ["access_level", "age"]
+employee = {"name": "John", "email": "john@ecorp.com", "access_level": 5, "age": 28}
+for removed in list_of_keys:
+    deleted_item = employee.pop(removed)
+print(employee)
